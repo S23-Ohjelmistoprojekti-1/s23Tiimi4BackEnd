0cb031d913821a039215a3d0cf7a92151b76f9ea
sannaroi <sanna.roine@hotmail.com>
2023-10-25 14:05:03 +0300

    Initial commit

--- dogbackend/src/main/java/com/example/dogbackend/DogbackendApplication.java
+++ dogbackend/src/main/java/com/example/dogbackend/DogbackendApplication.java
@@ -2,8 +2,12 @@ package com.example.dogbackend;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.dogbackend.domain.Vaate;
+import com.example.dogbackend.domain.VaateRepository;
 
 @SpringBootApplication
 public class DogbackendApplication {
@@ -13,5 +17,17 @@ public class DogbackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DogbackendApplication.class, args);
 	}
+	
+	public CommandLineRunner dogbackendDemo(VaateRepository repository) {
+		return (args) -> {
+			
+		repository.save(new Vaate(0, "Takki", "Punainen", "S", 21.00, "Rukka"));
+		
+		
+	};
+		
+	}
+	
+	}
 
-}
+
