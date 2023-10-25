cbc550cfbd87015327e07bce440aa6f0a32ac4fc
16cats <97065659+16cats@users.noreply.github.com>
2023-10-25 12:30:20 +0300

    first commit

---
+++ dogbackend/dogbackend/src/test/java/com/example/dogbackend/DogbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
