581c64b08c08f9be3048562a3b9b12c9b655dda1
Alex Lindholm <alexlindholm02@gmail.com>
2023-10-25 13:48:43 +0300

    controlleriin delete

--- dogbackend/src/main/java/com/example/dogbackend/web/DogController.java
+++ dogbackend/src/main/java/com/example/dogbackend/web/DogController.java
@@ -1,5 +1,21 @@
 package com.example.dogbackend.web;
 
-public class DogController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.dogbackend.domain.VaateRepository;
+
+@Controller
+public class DogController {
+	@Autowired
+	private VaateRepository repository;
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteVaate(@PathVariable Long id) {
+		repository.deleteById(id);
+		return "redirect:/itemlist";
+
+}
 }
