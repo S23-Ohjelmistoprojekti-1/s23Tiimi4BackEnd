2930d780372d7b15ff096fd11bf40d32f8d6f755
sannaroi <sanna.roine@hotmail.com>
2023-11-15 13:28:53 +0200

    Index

--- dogbackend/src/main/java/com/example/dogbackend/web/DogController.java
+++ dogbackend/src/main/java/com/example/dogbackend/web/DogController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,6 +27,11 @@ public class DogController {
 	private ValmistajaRepository vrepository;
 	@Autowired
 	private AsiakasRepository drepository;
+	
+	@GetMapping({"/", "index"})
+	public String showHome() {
+		return "index";
+	}
 	
 	@RequestMapping("/itemlist")
 	public String itemList(Model model) {
