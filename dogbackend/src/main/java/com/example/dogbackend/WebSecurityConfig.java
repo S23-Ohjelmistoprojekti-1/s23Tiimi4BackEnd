f6a9ce6be575001e826bcedea61a65d128aedf75
SpookySkoops <rautiainen.oskarir@gmail.com>
2023-11-20 14:00:18 +0200

    Ei enää estä frontendia toimimasta

--- dogbackend/src/main/java/com/example/dogbackend/WebSecurityConfig.java
+++ dogbackend/src/main/java/com/example/dogbackend/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.dogbackend;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,35 +10,50 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+ 
 import com.example.dogbackend.web.UserDetailsServiceImpl;
-
-
-
+ 
 @Configuration
-@EnableMethodSecurity(securedEnabled = true)
+@EnableMethodSecurity(securedEnabled = true) // SB 3.0
 public class WebSecurityConfig {
 	@Autowired
 	private UserDetailsServiceImpl userDetailsService;
-@Bean
-public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-	http
-		.authorizeHttpRequests()
-			.anyRequest().authenticated()
-			.and()
-		.formLogin()
-			.loginPage("/login")
-			.defaultSuccessUrl("/index",true)
-			.permitAll()
-			.and()
-		.httpBasic();
+ 
+	private static final AntPathRequestMatcher[] WHITE_LIST_URLS = { 
+			new AntPathRequestMatcher("/h2-console/**"),
+			new AntPathRequestMatcher("/api/**"),
+			new AntPathRequestMatcher("/vaatteet/**"),
+			new AntPathRequestMatcher("/asiakkaat/**"),
+			new AntPathRequestMatcher("/rekisterointi/**") };
+ 
+	private static final AntPathRequestMatcher[] ADMIN_LIST_URLS = { 
+			new AntPathRequestMatcher("/admin/**"),
+			new AntPathRequestMatcher("/owner/**") };
+ 
+	// secure configurations with lambda
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(authorizeRequest -> authorizeRequest
+				.requestMatchers(antMatcher("/css/**")).permitAll()
+				// .requestMatchers(antMatcher("/api/**")).permitAll()
+				.requestMatchers(antMatcher("/vaatteet/**")).permitAll()
+				.requestMatchers(antMatcher("/rekisterointi/**")).permitAll()
+				.requestMatchers(WHITE_LIST_URLS).permitAll().requestMatchers(ADMIN_LIST_URLS)
+				.hasAuthority("ADMIN")
+				.anyRequest().authenticated())
+				.headers(headers -> headers
+						.frameOptions(frameOptions -> frameOptions.disable())) // enable h2console
+				.formLogin(formlogin -> formlogin
+						.defaultSuccessUrl("/index", true).permitAll())
+				.logout(logout -> logout.permitAll())
+				.csrf(csrf -> csrf.disable()); // not for production, just for development
 		return http.build();
-}
-
-@Autowired
-public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-	auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-}
-
-
+	}
+ 
+ 
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
 }
