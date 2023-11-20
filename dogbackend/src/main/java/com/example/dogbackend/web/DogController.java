d519c7d0d4d0600c84fd0462f6e6b5c54ba2bdd5
SpookySkoops <rautiainen.oskarir@gmail.com>
2023-11-20 14:09:16 +0200

    typo fix

--- dogbackend/src/main/java/com/example/dogbackend/web/DogController.java
+++ dogbackend/src/main/java/com/example/dogbackend/web/DogController.java
@@ -1,6 +1,6 @@
 package com.example.dogbackend.web;
 
-import java.util.List;
+//import java.util.List;
 // import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.dogbackend.domain.Asiakas;
 import com.example.dogbackend.domain.AsiakasRepository;
